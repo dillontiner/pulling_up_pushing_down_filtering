08279b543d3c82ea93a2db299f6cb866f7e718de
Dillon Tiner <dillontiner@gmail.com>
2020-10-26 21:32:02 -0600

    make spec setup more legible

--- spec/pulling_up_pushing_down_filtering/interface_1/implementation_detail_spec.rb
+++ spec/pulling_up_pushing_down_filtering/interface_1/implementation_detail_spec.rb
@@ -1,6 +1,11 @@
 RSpec.describe PullingUpPushingDownFiltering::Interface1::ImplementationDetail do
   subject { described_class.filter(array_of_hashes) }
-  let(:array_of_hashes) { [{amount: 10, filter_for_this: true, filter_for_this_too: true}, {amount: 20, filter_for_this: false, filter_for_this_too: true}] }
+  let(:array_of_hashes) do
+    [
+        {amount: 10, filter_for_this: true, filter_for_this_too: true},
+        {amount: 20, filter_for_this: false, filter_for_this_too: true}
+    ]
+  end
 
   it "performs filtering" do
     expect(subject).to eq([{amount: 10, filter_for_this: true, filter_for_this_too: true}])
