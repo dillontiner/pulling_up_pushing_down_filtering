ac8d29e36fb047bf1676d390b8a24b0362a85003
Dillon Tiner <dillontiner@gmail.com>
2020-10-26 21:49:07 -0600

    [Green] Implement additional filtering in Interface1

--- lib/pulling_up_pushing_down_filtering/filter_sum_runner.rb
+++ lib/pulling_up_pushing_down_filtering/filter_sum_runner.rb
@@ -3,7 +3,7 @@ module PullingUpPushingDownFiltering
     class << self
       def filter_and_sum(array_of_hashes)
         filtered_hashes = array_of_hashes.select do |hash|
-          hash[:filter_for_this] == true
+          hash[:filter_for_this] == true || hash[:filter_for_this_too] == true
         end
 
         filtered_hashes.sum { |hash| hash[:amount] }
