36b79ae2db3345da41d191461038239fe9cca2d4
Dillon Tiner <dillontiner@gmail.com>
2020-10-26 21:41:41 -0600

    [Green] Implement filtering inline in Interface1

--- lib/pulling_up_pushing_down_filtering/filter_sum_runner.rb
+++ lib/pulling_up_pushing_down_filtering/filter_sum_runner.rb
@@ -2,7 +2,10 @@ module PullingUpPushingDownFiltering
   module FilterSumRunner
     class << self
       def filter_and_sum(array_of_hashes)
-        filtered_hashes = Filterer.filter(array_of_hashes)
+        filtered_hashes = array_of_hashes.select do |hash|
+          hash[:filter_for_this] == true
+        end
+
         filtered_hashes.sum { |hash| hash[:amount] }
       end
     end
