517827e63fb4adc60601d7e5b419ed1572668e17
Dillon Tiner <dillontiner@gmail.com>
2020-10-26 21:57:12 -0600

    update expectation description

--- spec/pulling_up_pushing_down_filtering/interface_2_spec.rb
+++ spec/pulling_up_pushing_down_filtering/interface_2_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe PullingUpPushingDownFiltering::Interface2 do
   subject { described_class.meddle_with_other_interfaces_detail }
 
-  it "filters input on filter_for_this and sums amount" do
+  it "calls implementation details behind the other interface" do
     expect(subject).to eq("I can reach call code the other interface >:)")
   end
 end
