b58defe70065c56e5f7c140e858e8b2a59b0a9d0
Dillon Tiner <dillontiner@gmail.com>
2020-10-26 21:53:03 -0600

    [Green] Implement filtering logic in ImplementationDetail

--- lib/pulling_up_pushing_down_filtering/filter_sum_runner.rb
+++ lib/pulling_up_pushing_down_filtering/filter_sum_runner.rb
@@ -2,10 +2,7 @@ module PullingUpPushingDownFiltering
   module FilterSumRunner
     class << self
       def filter_and_sum(array_of_hashes)
-        filtered_hashes = array_of_hashes.select do |hash|
-          hash[:filter_for_this] == true || hash[:filter_for_this_too] == true
-        end
-
+        filtered_hashes = Filterer.filter(array_of_hashes)
         filtered_hashes.sum { |hash| hash[:amount] }
       end
     end
--- lib/pulling_up_pushing_down_filtering/filter_sum_runner/filterer.rb
+++ lib/pulling_up_pushing_down_filtering/filter_sum_runner/filterer.rb
@@ -4,7 +4,7 @@ module PullingUpPushingDownFiltering
       class << self
         def filter(array_of_hashes)
           array_of_hashes.select do |hash|
-            hash[:filter_for_this] == true
+            hash[:filter_for_this] == true || hash[:filter_for_this_too] == true
           end
         end
       end
