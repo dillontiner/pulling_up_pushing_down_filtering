b37c22054a69c1802ce3ce3c936e68da43c5995b
Dillon Tiner <dillontiner@gmail.com>
2020-10-26 21:39:36 -0600

    [Red] Update Interface1's tests so it tests the filtering behavior performed by ImplementationDetail, comment out Interface1's use of ImplementationDetail

--- spec/pulling_up_pushing_down_filtering/filter_sum_runner_spec.rb
+++ spec/pulling_up_pushing_down_filtering/filter_sum_runner_spec.rb
@@ -1,5 +1,11 @@
 RSpec.describe PullingUpPushingDownFiltering::FilterSumRunner do
-  subject { described_class.filter_and_sum([{amount: 10, filter_for_this: true, filter_for_this_too: true}]) }
+  subject { described_class.filter_and_sum(array_of_hashes) }
+  let(:array_of_hashes) do
+    [
+        {amount: 10, filter_for_this: true, filter_for_this_too: true},
+        {amount: 20, filter_for_this: false, filter_for_this_too: true}
+    ]
+  end
 
   it "filters input on filter_for_this and sums amount" do
     expect(subject).to eq(10)
