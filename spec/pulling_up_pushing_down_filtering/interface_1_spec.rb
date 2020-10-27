064a4b6a215a7f8002a201a5f8e532beddc08350
Dillon Tiner <dillontiner@gmail.com>
2020-10-26 21:24:52 -0600

    change setup to not stub

--- spec/pulling_up_pushing_down_filtering/interface_1_spec.rb
+++ spec/pulling_up_pushing_down_filtering/interface_1_spec.rb
@@ -1,9 +1,5 @@
 RSpec.describe PullingUpPushingDownFiltering::Interface1 do
-  subject { described_class.filter_and_sum([]) }
-
-  before do
-    allow(described_class::ImplementationDetail).to receive(:filter).and_return([{amount: 10, filter_for_this: true}])
-  end
+  subject { described_class.filter_and_sum([{amount: 10, filter_for_this: true}]) }
 
   it "filters input on filter_for_this and sums amount" do
     expect(subject).to eq(10)
