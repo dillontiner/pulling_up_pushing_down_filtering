ec56fb1f6a53ed88e5df6314f075de9712d20bde
Dillon Tiner <dillontiner@gmail.com>
2020-10-26 21:42:27 -0600

    rename a variable for consistency with other classes

--- lib/pulling_up_pushing_down_filtering/interface_1/implementation_detail.rb
+++ lib/pulling_up_pushing_down_filtering/interface_1/implementation_detail.rb
@@ -2,9 +2,9 @@ module PullingUpPushingDownFiltering
   module Interface1
     module ImplementationDetail
       class << self
-        def filter(array_of_objects)
-          array_of_objects.select do |object|
-            object[:filter_for_this] == true
+        def filter(array_of_hashes)
+          array_of_hashes.select do |hash|
+            hash[:filter_for_this] == true
           end
         end
       end
