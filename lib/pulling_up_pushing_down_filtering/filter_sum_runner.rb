b8b258e2d67dce970b866552f2a18207dd80c185
Dillon Tiner <dillontiner@gmail.com>
2020-10-26 21:40:48 -0600

    [Green] Uncomment Interface1's use of ImplementationDetail

--- lib/pulling_up_pushing_down_filtering/filter_sum_runner.rb
+++ lib/pulling_up_pushing_down_filtering/filter_sum_runner.rb
@@ -2,8 +2,8 @@ module PullingUpPushingDownFiltering
   module FilterSumRunner
     class << self
       def filter_and_sum(array_of_hashes)
-        # filtered_hashes = Filterer.filter(array_of_hashes)
-        array_of_hashes.sum { |hash| hash[:amount] }
+        filtered_hashes = Filterer.filter(array_of_hashes)
+        filtered_hashes.sum { |hash| hash[:amount] }
       end
     end
   end
